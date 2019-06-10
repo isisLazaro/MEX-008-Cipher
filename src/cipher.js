7a62a5949bf41a89a4973fa4eba38a081050f139
Isis Lázaro <isislazaro@gmail.com>
2019-06-10 17:10:55 -0500

    cipher ya está exportado al objeto global window

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,18 @@
 window.cipher = {
-  // ... 
-};
-
-const encode = (offset, text) =>{
-  text = text.toUpperCase(); // converts the text to uppercase letters
-  // FALTA QUITAR CARACTERES ESPECIALES: ACENTOS, Ñ, SIMBOLOS
-  let encryptedChar = "";
-  let encryptedText = "";
-
-  for (let i = 0; i < text.length; i++) {
-      if (text[i] !== " ") { //anything but white space
-          encryptedChar = String.fromCharCode(((text.charCodeAt(i) - 65 + offset) % 26) + 65);
-      } else { // keep white space
-          encryptedChar = text[i];
-      }
-      encryptedText = encryptedText + encryptedChar; 
+  encode : (offset, text) =>{
+    text = text.toUpperCase(); // converts the text to uppercase letters
+    // FALTA QUITAR CARACTERES ESPECIALES: ACENTOS, Ñ, SIMBOLOS
+    let encryptedChar = "";
+    let encryptedText = "";
+  
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] !== " ") { //anything but white space
+            encryptedChar = String.fromCharCode(((text.charCodeAt(i) - 65 + offset) % 26) + 65);
+        } else { // keep white space
+            encryptedChar = text[i];
+        }
+        encryptedText = encryptedText + encryptedChar; 
+    }
+    return encryptedText;
   }
-  return encryptedText;
-}
-
-var cipher = {}; //objeto
-//console.log(typeof cipher);
-cipher.encode = encode;
+};
---
+++ src/index.js
@@ -0,0 +1,1 @@
+//window.cipher.encode(3, "ABCD");
