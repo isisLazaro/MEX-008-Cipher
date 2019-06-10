77cfb9dc46105e11bce64cb8c500e56d4708ab99
Isis Lázaro <isislazaro@gmail.com>
2019-06-10 16:08:20 -0500

    implementación objeto cipher con método encode. Fata exportarl al objeto global

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,24 @@
 window.cipher = {
   // ... 
 };
+
+const encode = (offset, text) =>{
+  text = text.toUpperCase(); // converts the text to uppercase letters
+  // FALTA QUITAR CARACTERES ESPECIALES: ACENTOS, Ñ, SIMBOLOS
+  let encryptedChar = "";
+  let encryptedText = "";
+
+  for (let i = 0; i < text.length; i++) {
+      if (text[i] !== " ") { //anything but white space
+          encryptedChar = String.fromCharCode(((text.charCodeAt(i) - 65 + offset) % 26) + 65);
+      } else { // keep white space
+          encryptedChar = text[i];
+      }
+      encryptedText = encryptedText + encryptedChar; 
+  }
+  return encryptedText;
+}
+
+var cipher = {}; //objeto
+//console.log(typeof cipher);
+cipher.encode = encode;
