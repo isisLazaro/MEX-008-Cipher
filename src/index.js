6c71d223d2806787cb82e3a7a606272ad07b5e24
Isis Lázaro <isislazaro@gmail.com>
2019-06-11 15:09:11 -0500

    html ya tiene textarea y cifrar button funcionando, falta refinar funcionamiento

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-//window.cipher.encode(3, "ABCD");
+
+let form1 = document.getElementById("form-1");
+let spoilerArea = document.getElementById("spoiler-area");
+let encodeButton = document.getElementById("encode-button");
+
+form1.addEventListener("submit", event => {
+    //console.log(cipher.encode(1,spoilerArea.value));
+    spoilerArea.value = cipher.encode(19,spoilerArea.value);
+    event.preventDefault()
+});
+
