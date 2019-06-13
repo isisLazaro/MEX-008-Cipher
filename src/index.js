d95f49f0e631333dfd93639e72878a93538d6238
Isis Lázaro <isislazaro@gmail.com>
2019-06-12 23:06:00 -0500

    usuario ya puede introducir valor de offset, es obligatorio escribirlo

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@
 let form1 = document.getElementById("form-1");
 let spoilerArea = document.getElementById("spoiler-area");
 let offsetText = document.getElementById("offset-text");
-let encodeButton = document.getElementById("encode-button");
+//let encodeButton = document.getElementById("encode-button");
 
 spoilerArea.focus();
 
 form1.addEventListener("submit", event => {
     //console.log(cipher.encode(1,spoilerArea.value));
-    //offsetText.value
-    spoilerArea.value = cipher.encode(1,spoilerArea.value);
+    spoilerArea.value = cipher.encode(Number(offsetText.value),spoilerArea.value);
     event.preventDefault()
 });
 
