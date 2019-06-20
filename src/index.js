28c20d2c9e88d811fb7c7d6a02b0ead85c59f3c9
Isis Lázaro <isislazaro@gmail.com>
2019-06-20 07:41:14 -0500

    se modificó index.js para decifrar y se desactivo autocomplete en el html

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ let form1 = document.getElementById("form-1");
 let spoilerArea = document.getElementById("spoiler-area");
 let offsetText = document.getElementById("offset-text");
 //let encodeButton = document.getElementById("encode-button");
-
 spoilerArea.focus();
+
+let form2 = document.getElementById("form-2");
+let spoilerArea2 = document.getElementById("spoiler-area-2");
+let offsetText2 = document.getElementById("offset-text-2");
+spoilerArea2.focus();
 
 form1.addEventListener("submit", event => {
     spoilerArea.value = cipher.encode(Number(offsetText.value),spoilerArea.value);
     event.preventDefault()
 });
 
+form2.addEventListener("submit", event2 => {
+    spoilerArea2.value = cipher.decode(Number(offsetText2.value),spoilerArea2.value);
+    event2.preventDefault()
+});
